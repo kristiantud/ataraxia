d797fd45c9533392903c7b3dce874dd7f886a9ee
kristiantud <kristiantud@hotmail.com>
2021-04-22 03:03:17 -0600

    Added designs on the cards, fixed colors, added another background and removed one

--- change_background.js
+++ change_background.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var num = Math.floor(Math.random() * 6);
     var vid1_source = "video_backgrounds/background_mountains.mp4";
     var vid2_source = "video_backgrounds/background_beach.mp4";
-    var vid3_source = "video_backgrounds/background_rain.mp4";
+    var vid3_source = "video_backgrounds/background_waters.mp4";
     var vid4_source = "video_backgrounds/background_rain2.mp4";
     var vid5_source = "video_backgrounds/background_waterfall.mp4";
     var vid6_source = "video_backgrounds/background_ladysunset.mp4";
@@ -23,27 +23,33 @@ window.onload = function () {
         // $("#background_video source")[0].load();
         
     } else if (num == 2){
-        // play rain
+        // play waters
         $('#background_video').animate({opacity : '1' },2000);
         $("#background_video").html('<source src=' + vid3_source  +' type="video/mp4"></source>' );
+        $('.title_content').css({color:'#151515'}); 
+        $('.title_content .scroll_down').css({color: '#b4a5a5'});
         // $("#background_video source")[0].load();
        
     } else if (num == 3){
         // play rain2
         $('#background_video').animate({opacity : '1' },2000);
         $("#background_video").html('<source src=' + vid4_source  +' type="video/mp4"></source>' );
+        $('.title_content').css({color:'#b4a5a5'}); 
         // $("#background_video source")[0].load();
         
     } else if (num == 4) {
         // play waterfall
         $('#background_video').animate({opacity : '1' },2000);
         $("#background_video").html('<source src=' + vid5_source  +' type="video/mp4"></source>' );
+        $('.title_content').css({color:'#151515'}); 
+        $('.title_content .scroll_down').css({color: '#b4a5a5'});
         // $("#background_video source")[0].load();  
        
     } else {
         // play lake
         $('#background_video').animate({opacity : '1' },2000);
         $("#background_video").html('<source src=' + vid6_source  +' type="video/mp4"></source>' );
+        $('.title_content').css({color:'#b4a5a5'}); 
         // $("#background_video source")[0].load();  
         
     }
