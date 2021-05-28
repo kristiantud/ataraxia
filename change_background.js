4bfc902b002baef95b15404c537497951a81d14a
kristiantud <kristiantud@hotmail.com>
2021-05-27 18:25:19 -0600

    changed styling

--- change_background.js
+++ change_background.js
@@ -5,7 +5,7 @@ window.onload = function () {
     var vid3_source = "video_backgrounds/background_waters.mp4";
     var vid4_source = "video_backgrounds/background_rain2.mp4";
     var vid5_source = "video_backgrounds/background_waterfall.mp4";
-    var vid6_source = "video_backgrounds/background_fieldrain.mp4";
+    var vid6_source = "video_backgrounds/background_mountain.mp4";
     if (num == 0){
         // play mountains
         $('#background_video').animate({opacity : '1' },2000);
